cf5c08e158ce13b0e0d437676afd9774258243f6
David Kha <david.kha44@gmail.com>
2021-01-05 14:24:10 +0100

    Add open-sans as custom font

--- App.js
+++ App.js
@@ -2,8 +2,23 @@ import "react-native-gesture-handler";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  OpenSans_400Regular,
+  OpenSans_700Bold,
+} from "@expo-google-fonts/open-sans";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <NavigationContainer>
       <View style={styles.container}>
