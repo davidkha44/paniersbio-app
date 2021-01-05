37f1a67f2e8c317fffe81468d8410a824f9a7d36
David Kha <david.kha44@gmail.com>
2021-01-05 15:53:28 +0100

    Add dummy data for the weekly veggies
    DEV ONLY

---
+++ data/weeklyVeggies-data.js
@@ -0,0 +1,30 @@
+import weeklyVeggies from "../models/weeklyVeggies";
+
+const WEEKLYVEGGIES = [
+  new weeklyVeggies(
+    1,
+    "Carrots",
+    "https://images.pexels.com/photos/3650647/pexels-photo-3650647.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+    "200g"
+  ),
+  new weeklyVeggies(
+    2,
+    "Garlic",
+    "https://images.pexels.com/photos/10677/Garlic-free-license-CC0.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "200g"
+  ),
+  new weeklyVeggies(
+    3,
+    "Potatoes",
+    "https://images.pexels.com/photos/3650647/pexels-photo-3650647.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+    "500g"
+  ),
+  new weeklyVeggies(
+    4,
+    "Tomatoes",
+    "https://images.pexels.com/photos/533280/pexels-photo-533280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "300g"
+  ),
+];
+
+export default WEEKLYVEGGIES;
---
+++ models/weeklyVeggies.js
@@ -0,0 +1,10 @@
+class weeklyVeggies {
+  constructor(_id, name, imageUrl, quantity) {
+    this._id = _id;
+    this.name = name;
+    this.imageUrl = imageUrl;
+    this.quantity = quantity;
+  }
+}
+
+export default weeklyVeggies;
