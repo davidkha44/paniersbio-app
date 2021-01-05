1ca9e552fa8f7141d58da79ac645142af2aa3048
David Kha <david.kha44@gmail.com>
2021-01-05 14:37:32 +0100

    Add tab navigation

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import "react-native-gesture-handler";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { Provider as PaperProvider } from "react-native-paper";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import AppLoading from "expo-app-loading";
 import {
   useFonts,
   OpenSans_400Regular,
   OpenSans_700Bold,
 } from "@expo-google-fonts/open-sans";
+
+import AppNavigation from "./components/AppNavigation";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -20,19 +24,10 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <Text>This is a blank template</Text>
-      </View>
-    </NavigationContainer>
+    <PaperProvider settings={{ icon: (props) => <Ionicons {...props} /> }}>
+      <NavigationContainer>
+        <AppNavigation />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ components/AppNavigation.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Colors from "../constants/Colors";
+
+import HomeScreen from "../screens/HomeScreen";
+import SubscriptionScreen from "../screens/SubscriptionScreen";
+import FavouriteScreen from "../screens/FavouriteScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+
+const Tab = createMaterialBottomTabNavigator();
+
+const AppNavigation = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconName;
+          let iconColor = focused ? Colors.primary : Colors.inactive;
+          switch (route.name) {
+            case "Home":
+              iconName = "home";
+              break;
+            case "Subscription":
+              iconName = "basket";
+              break;
+            case "Favourites":
+              iconName = "heart";
+              break;
+            case "Settings":
+              iconName = "settings";
+              break;
+            default:
+              iconName = "";
+              break;
+          }
+          return <Ionicons name={iconName} color={iconColor} size={24} />;
+        },
+      })}
+      tabBarOptions={{
+        labelStyle: { fontFamily: "OpenSans_400Regular" },
+      }}
+      activeColor={Colors.primary}
+      inactiveColor={Colors.inactive}
+      barStyle={{ backgroundColor: "white" }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Subscription" component={SubscriptionScreen} />
+      <Tab.Screen name="Favourites" component={FavouriteScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default AppNavigation;
