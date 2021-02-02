c81b9cb1480d05de0fde8699922da5b223392efd
David Kha <david.kha44@gmail.com>
2021-02-02 14:30:49 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text>Hello</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
