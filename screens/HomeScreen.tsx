fee17d6190b40d675364201e45afa4456328af52
David Kha <david.kha44@gmail.com>
2021-02-16 11:11:31 +0100

    Add scrollview to home screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 
 import WeeklyVeg from '../components/Home/WeeklyVeg';
 import WeeklyRecipe from '../components/Home/WeeklyRecipe';
@@ -7,8 +7,10 @@ import WeeklyRecipe from '../components/Home/WeeklyRecipe';
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <WeeklyVeg />
-      <WeeklyRecipe />
+      <ScrollView>
+        <WeeklyVeg />
+        <WeeklyRecipe />
+      </ScrollView>
     </View>
   );
 };
@@ -16,7 +18,6 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: '40%',
   },
 });
 
