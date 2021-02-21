e512a739aaf48bfcde583c0f566c4c2f81c0b990
David Kha <david.kha44@gmail.com>
2021-02-21 16:41:34 +0100

    Add general layout of the settings screen

--- screens/App/SettingsScreen.tsx
+++ screens/App/SettingsScreen.tsx
@@ -6,13 +6,43 @@ const SettingsScreen = () => {
   const auth = useContext(AuthContext);
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Général</Text>
+      <TouchableNativeFeedback>
+        <View style={styles.parameter}>
+          <Text>Notifications</Text>
+        </View>
+      </TouchableNativeFeedback>
+      <TouchableNativeFeedback>
+        <View style={styles.parameter}>
+          <Text>Thème</Text>
+        </View>
+      </TouchableNativeFeedback>
+
+      <Text style={styles.title}>Contact</Text>
+      <TouchableNativeFeedback>
+        <View style={styles.parameter}>
+          <Text>Email Les Paniers Bio Support</Text>
+        </View>
+      </TouchableNativeFeedback>
+
       <Text style={styles.title}>Compte</Text>
       <TouchableNativeFeedback onPress={() => auth.logout()}>
         <View style={styles.parameter}>
           <Text style={styles.paramText}>Se déconnecter</Text>
         </View>
       </TouchableNativeFeedback>
+
       <Text style={styles.title}>A propos</Text>
+      <TouchableNativeFeedback>
+        <View style={styles.parameter}>
+          <Text>Termes et conditions</Text>
+        </View>
+      </TouchableNativeFeedback>
+      <TouchableNativeFeedback>
+        <View style={styles.parameter}>
+          <Text>Politique de confidentialité</Text>
+        </View>
+      </TouchableNativeFeedback>
     </View>
   );
 };
@@ -33,6 +63,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingLeft: '5%',
     borderRadius: 5,
+    marginVertical: 2,
   },
   paramText: {
     fontFamily: 'OpenSans-SemiBold',
