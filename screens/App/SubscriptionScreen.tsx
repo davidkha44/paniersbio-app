ee6cf18958bcf2edb6536aacc82d6bea39cdb13c
David Kha <david.kha44@gmail.com>
2021-03-01 08:59:37 +0100

    Layout of the subscription screen

--- screens/App/SubscriptionScreen.tsx
+++ screens/App/SubscriptionScreen.tsx
@@ -1,19 +1,70 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import Card from '../../components/Card';
+
+interface subs {
+  _id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+}
 
 const SubscriptionScreen = () => {
+  const renderVegCard = ({ item }: { item: subs }) => {
+    const { name, price, imageUrl } = item;
+    return <Card name={name} quantity={price} imageUrl={imageUrl} />;
+  };
+  const SUBSCRIPTION: subs[] = [
+    {
+      _id: 1,
+      name: 'Panier unitaire',
+      price: 5,
+      imageUrl:
+        'https://images.pexels.com/photos/1300972/pexels-photo-1300972.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    },
+    {
+      _id: 2,
+      name: 'Abonnement 1 mois',
+      price: 22,
+      imageUrl:
+        'https://images.pexels.com/photos/1300972/pexels-photo-1300972.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    },
+    {
+      _id: 3,
+      name: 'Abonnnement 4 mois',
+      price: 80,
+      imageUrl:
+        'https://images.pexels.com/photos/1300972/pexels-photo-1300972.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    },
+  ];
   return (
-    <View style={styles.container}>
-      <Text>This is the subscription screen</Text>
+    <View>
+      <Image
+        source={require('../../assets/images/shipping.png')}
+        style={styles.image}
+      />
+      <Text style={styles.title}>Abonnements</Text>
+      <FlatList
+        data={SUBSCRIPTION}
+        renderItem={renderVegCard}
+        keyExtractor={(item) => item._id.toString()}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  image: {
+    width: '100%',
+    height: 240,
+    resizeMode: 'cover',
+    marginBottom: 20,
+  },
+  title: {
+    paddingLeft: '10%',
+    fontFamily: 'OpenSans-Bold',
+    fontSize: 18,
+    marginVertical: 10,
   },
 });
 
