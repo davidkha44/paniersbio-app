1612a91e8edf7875a90c9b7597cbc341a034d68f
David Kha <david.kha44@gmail.com>
2021-03-01 09:01:51 +0100

    Layout of the recipe screen

--- screens/App/RecipeScreen.tsx
+++ screens/App/RecipeScreen.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const RecipeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>This is the recipe screen</Text>
+    <View>
+      <Image
+        source={require('../../assets/images/pizza-share.png')}
+        style={styles.image}
+      />
+      <Text style={styles.title}>Favoris</Text>
+      <Text style={styles.title}>Recette de la semaine</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  image: {
+    width: '100%',
+    height: 240,
+    resizeMode: 'cover',
+    marginBottom: 20,
+  },
+  title: {
+    paddingLeft: '10%',
+    fontFamily: 'OpenSans-Bold',
+    fontSize: 18,
+    marginVertical: 10,
   },
 });
 
